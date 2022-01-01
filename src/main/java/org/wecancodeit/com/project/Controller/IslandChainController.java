ccd5347874551384a249c5b7a073173bba2caf42
CamS-World <cstephen13@mail.bw.edu>
2022-01-01 17:17:42 -0500

    Island Chain Controller: request mapping and get mapping. Island Chain Repo: extend crud repository

--- src/main/java/org/wecancodeit/com/project/Controller/IslandChainController.java
+++ src/main/java/org/wecancodeit/com/project/Controller/IslandChainController.java
@@ -1,4 +1,33 @@
 package org.wecancodeit.com.project.Controller;
 
-public class IslandChainController {
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.wecancodeit.com.project.Model.IslandChainModel;
+import org.wecancodeit.com.project.Repo.IslandChainRepo;
+
+import javax.annotation.Resource;
+
+@Controller
+public class IslandChainController  {
+
+    @Resource
+    private IslandChainRepo islandChainRepo;
+
+    @RequestMapping("/island-chains")
+    public String displayIslandChains(Model model){
+        model.addAttribute("island-chains", islandChainRepo.findAll());
+        return "islandChainTemplate";
+    }
+
+    @GetMapping("/island-chains/{islandName}")
+    public String displaySingleCampus(@PathVariable String islandName, Model model){
+        IslandChainModel retrievedIslandChain = islandChainRepo.findIslandChainByName(islandName);
+        model.addAttribute("island-chain",retrievedIslandChain);
+        return "islandChainView";
+    }
+
 }
