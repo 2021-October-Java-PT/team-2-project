1f4a307f4306cadf2385b075304f775c153a7b42
CamS-World <92181988+CamS-World@users.noreply.github.com>
2022-01-02 17:47:00 -0500

    Update WaterBodyController.java

--- src/main/java/org/wecancodeit/com/project/Controller/WaterBodyController.java
+++ src/main/java/org/wecancodeit/com/project/Controller/WaterBodyController.java
@@ -9,7 +9,6 @@ import org.wecancodeit.com.project.Model.WaterBodyModel;
 import org.wecancodeit.com.project.Repo.WaterBodyRepo;
 
 import javax.annotation.Resource;
-import java.util.Collections;
 
 @Controller
 public class WaterBodyController {
@@ -23,16 +22,10 @@ public class WaterBodyController {
         return "allWaterBodiesTemplate";
     }
 
-//    @GetMapping("/waterBodies/{id}")
-//    public String displaySingleBodyOfWater(@PathVariable Long id, Model model) {
-//        WaterBodyModel retrievedWaterBody = (WaterBodyModel) waterBodyRepository.findAllById(Collections.singleton(id));
-//        model.addAttribute("waterBody", retrievedWaterBody);
-//        return "waterBodyTemplate";
-//    }
 
     @GetMapping("/waterBodies/{waterBodyName}")
     public String displaySingleBodyOfWater(@PathVariable String waterBodyName, Model model) {
-        WaterBodyModel retrievedWaterBody = (WaterBodyModel) waterBodyRepository.findWaterBodyModelByWaterBodyName(waterBodyName);
+        WaterBodyModel retrievedWaterBody = waterBodyRepository.findWaterBodyModelByWaterBodyName(waterBodyName);
         model.addAttribute("waterBody", retrievedWaterBody);
         return "waterBodyTemplate";
     }
