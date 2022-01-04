4564e6da52ba505a3b02f822998d429eb2cb45bb
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-01-04 12:21:27 -0500

    Added IndexController for the homepage

--- src/main/java/org/wecancodeit/com/project/Controller/IndexController.java
+++ src/main/java/org/wecancodeit/com/project/Controller/IndexController.java
@@ -1,2 +1,12 @@
-package org.wecancodeit.com.project.Controller;public class IndexController {
+package org.wecancodeit.com.project.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/home","/", ""})
+    public String displayHomePage(){
+        return "index";
+    }
 }
