a89d70506ba68495b982388313ecac17fbe89b56
Madison Mitchell <82128038+madison-mitchell@users.noreply.github.com>
2022-01-05 09:44:49 -0500

    Update IndexController.java
    Added back in "/home" for URL mapping

--- src/main/java/org/wecancodeit/com/project/Controller/IndexController.java
+++ src/main/java/org/wecancodeit/com/project/Controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping({"/", ""})
+    @RequestMapping({"/home", "/", ""})
     public String displayHomePage(){
         return "index";
     }
