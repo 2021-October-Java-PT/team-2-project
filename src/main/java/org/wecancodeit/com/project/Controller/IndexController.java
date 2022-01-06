1787f02346fa38b85dce8a99dc3478da5fdc9811
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-01-06 14:05:58 -0500

    Updates to Nav Bar & CSS

--- src/main/java/org/wecancodeit/com/project/Controller/IndexController.java
+++ src/main/java/org/wecancodeit/com/project/Controller/IndexController.java
@@ -1,10 +1,21 @@
 package org.wecancodeit.com.project.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.wecancodeit.com.project.Model.IslandChainModel;
+import org.wecancodeit.com.project.Repo.IslandChainRepo;
+
+import javax.annotation.Resource;
+import java.util.Optional;
 
 @Controller
 public class IndexController {
+
+    @Resource
+    private IslandChainRepo islandChainRepo;
+
     @RequestMapping({"/home", "/", ""})
     public String displayHomePage() {
         return "index";
